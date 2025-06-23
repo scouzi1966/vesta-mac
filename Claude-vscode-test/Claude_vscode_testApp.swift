06852f6d6f3fbc41f996870be2c6ff17b4520c91
Sylvain Cousineau <sylvain.cousineau@gmail.com>
2025-06-23 13:40:09 -0400

    Initial Commit

---
+++ Claude-vscode-test/Claude_vscode_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_vscode_testApp.swift
+//  Claude-vscode-test
+//
+//  Created by Sylvain Cousineau on 2025-06-23.
+//
+
+import SwiftUI
+
+@main
+struct Claude_vscode_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
